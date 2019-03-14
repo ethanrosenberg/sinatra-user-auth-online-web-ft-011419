4d1a6ec95185efac1dd719e56b9657eb3b1a4cf3
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-03-14 04:49:30 +0000

    Automatically backed up by Learn

--- db/migrate/20190314041815_users.rb
+++ db/migrate/20190314041815_users.rb
@@ -1,6 +1,8 @@
 class Users < ActiveRecord::Migration
   def change
-    create_table :users do |t|
+    add_column :name, :string
+    add_column :email, :string
+    add_column :password, :string
       t.string :name
       t.string :email
       t.string :password
